9844571eee6b5ad9b850214e57c761de9be1ebac
aquasky <aquasky0323@gmail.com>
2015-08-16 23:27:57 +0900

    create problem1 src

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "problem/problem.h"
+
+int main(int argc, char *argv[]) {
+	solve_0001();
+	return 0;
+}
+
+
---
+++ problem/problem_0001.cpp
@@ -0,0 +1,7 @@
+#include "common.h"
+
+void solve_0001() {
+	printf("Solve 0001\n");
+}
+
+
